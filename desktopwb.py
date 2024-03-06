0acc912e6af57008a9ca8391456ea2b800ff5572
Thomas Way <thomasgregoryway@gmail.com>
2024-03-06 15:01:00 -0500

    Added monitor resolution and exit keybinds

--- desktopwb.py
+++ desktopwb.py
@@ -1,17 +1,19 @@
-# Example file showing a basic pygame "game loop"
 import pygame
+import pygame.locals
+from screeninfo import get_monitors
 
 #GET SCREEN SIZE
+screenX = get_monitors()[0].width
+screenY = get_monitors()[0].height
 
-#CHECK IF BACKGROUND IMAGE EXISTS
-#IF NOT, CREATE BLANK CANVAS
-#IF BACKGROUND IMAGE EXISTS, LOAD IT TO CANVAS
+#TRY LOADING wallpaper.png FROM SOURCE (PREVIOUS DESKTOPWB SESSION)
+#IF ERR, CREATE BLANK WALLPAPER AND BACKUP CURRENT WALLPAPERS
 
 
 # pygame setup
 pygame.init()
 #update to open canvas the size of the monitor
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((screenX, screenY-50))
 clock = pygame.time.Clock()
 running = True
 
@@ -26,8 +28,21 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        #DRAWING
         elif pygame.mouse.get_pressed()[0]:
             pygame.draw.circle(screen, pygame.Color(255,255,255), pygame.mouse.get_pos(), 5)
+
+        #CHECKING FOR KEYPRESS
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                print("Exiting without saving")
+                running = False
+            #CTRL + s to save and exit
+            elif event.key == pygame.K_s and pygame.key.get_mods() & pygame.KMOD_CTRL:
+                print("Exiting and saving")
+                #update wallpaper
+                running = False
+
 
     # fill the screen with a color to wipe away anything from last frame
     #screen.fill("purple")
@@ -42,3 +57,4 @@ while running:
     clock.tick(60)  # limits FPS to 60
 
 pygame.quit()
+
